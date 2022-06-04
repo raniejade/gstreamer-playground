ef7e7c19f580370f70a00be96285ebc02d7ae718
Ranie Ramiso <raniejaderamiso@gmail.com>
2022-06-04 15:58:13 +1000

    Add test for scaling

--- scale/src/main.rs
+++ scale/src/main.rs
@@ -9,22 +9,40 @@ fn main() {
         .expect("Could not create source element.");
     let convert = gst::ElementFactory::make("videoconvert", Some("convert"))
         .expect("Could not create convert element.");
-    let sink = gst::ElementFactory::make("autovideosink", Some("sink"))
-        .expect("Could not create sink element");
+    let scale = gst::ElementFactory::make("videoscale", Some("scale"))
+        .expect("Could not create scale element");
+    let encode = gst::ElementFactory::make("x264enc", Some("encode"))
+        .expect("Could not create encode element");
+    let mp4mux =
+        gst::ElementFactory::make("mp4mux", Some("mux")).expect("Could not create mux element");
+    let sink =
+        gst::ElementFactory::make("filesink", Some("sink")).expect("Could not create sink element");
 
     // Build the pipeline
     let uri = "file:///home/raniejade/Downloads/sintel_trailer-480p.webm";
-    // let pipeline = gst::parse_launch(&format!("playbin uri={}", uri)).unwrap();
 
     // Create the empty pipeline
     let pipeline = gst::Pipeline::new(Some("test-pipeline"));
 
     // Build the pipeline
-    pipeline.add_many(&[&source, &convert, &sink]).unwrap();
-    gst::Element::link_many(&[&convert, &sink]).expect("Elements could not be linked.");
+    pipeline
+        .add_many(&[&source, &convert, &scale, &encode, &mp4mux, &sink])
+        .unwrap();
+    convert.link(&scale).expect("Failed to link convert ! scale");
+    scale.link_filtered(
+        &encode,
+        &gst::Caps::builder("video/x-raw")
+            .field("height", 320i32)
+            .build(),
+    ).expect("Failed to set caps filter for scale");
+
+    gst::Element::link_many(&[&encode, &mp4mux, &sink])
+        .expect("encode ! mp4mux ! sink could not be linked.");
+
 
     // Modify the source's properties
     source.set_property_from_str("uri", uri);
+    sink.set_property_from_str("location", "scaled.mp4");
 
     // Connect the pad-added signal
     source.connect_pad_added(move |src, src_pad| {
